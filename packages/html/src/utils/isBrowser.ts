b0df1c8502fbf320397f5e87413cad6884a4894a
Raya Straus <raya@cloudinary.com>
2021-11-14 16:37:38 +0200

    added html and react

---
+++ packages/html/src/utils/isBrowser.ts
@@ -0,0 +1,6 @@
+/**
+ * return true when window is defined
+ */
+export function isBrowser(): boolean{
+  return typeof window !== 'undefined';
+}
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,11 @@
+import {
+  placeholder,
+  accessibility,
+  lazyload,
+  responsive
+} from '@cloudinary/html'
+import { AdvancedImage } from './AdvancedImage';
+import { AdvancedVideo } from './AdvancedVideo';
+import { AdvancedPicture } from './AdvancedPicture';
+
+export { placeholder, accessibility, lazyload, responsive, AdvancedImage, AdvancedVideo, AdvancedPicture };
